a1bf002458276e431da90ee69aa0dd08d96ed606
Ansh2004P <anshkpatel15@gmail.com>
2025-03-03 01:03:32 +0530

    Step3-PreviewPage done

--- src/config/products.ts
+++ src/config/products.ts
@@ -1,13 +1,13 @@
 export const PRODUCT_PRICES = {
     material: {
-      silicone: 0,
-      polycarbonate: 5_00,
+      silicone: 100_00,
+      polycarbonate: 150_00,
     },
     finish: {
-      smooth: 0,
-      textured: 3_00,
+      smooth: 20,
+      textured: 40_00,
     },
   } as const
   
-  export const BASE_PRICE = 14_00
+  export const BASE_PRICE = 100_00
   
